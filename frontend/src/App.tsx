7868a895c830c1383c6626cf1926cccb7a007bb8
dai-pom <dai.pomew.05.28@gmail.com>
2020-01-26 16:46:58 +0900

    kari

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Login from "./containers/Login";
+import About from "./containers/About";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  NavLink
+} from "reactstrap";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Menu />
+          <Switch>
+            <Route exact path={"/"} component={Login} />
+            <Route path={"/about"} component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
+type MenuProps = {};
+const Menu: React.FC<MenuProps> = () => {
+  return (
+    <div>
+      <Navbar expand="md">
+        <NavbarBrand href="/">Todo</NavbarBrand>
+        <Nav navbar>
+          <NavItem>
+            <NavLink herf="/">HOME</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>option1</DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
